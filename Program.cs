45cc2bbc980b855a0ea32b3f369412a3b9a1bbdc
INGRESSO\thiago.paula <3rd.thiago.paula@ingresso.com>
2018-09-23 21:17:40 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -14,12 +14,23 @@ namespace SampleWebApi
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+            host.Run();
+
+            //BuildWebHost(args).Run();
+        }
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
     }
 }
